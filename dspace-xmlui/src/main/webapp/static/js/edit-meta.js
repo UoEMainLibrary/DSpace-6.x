c5ebeed56c3dd27e1c1b7765cc7628010dc01bdb
j4bbi <hrafn.malmquist@gmail.com>
2019-10-09 15:22:35 +0100

    Reorganise dropdown so that most used are top. CSS hack to move language down (by expanding textarea) Add default language value

---
+++ dspace-xmlui/src/main/webapp/static/js/edit-meta.js
@@ -0,0 +1,3 @@
+var text_class = $('#aspect_administrative_item_EditItemMetadataForm_field_value').parent().parent().attr("class");
+
+$('#aspect_administrative_item_EditItemMetadataForm_field_value').parent().parent().attr("class", text_class.replace('6', '12'));
