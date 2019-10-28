f3d9e43d6db762a5ddfec99006144abcabaa1638
j4bbi <hrafn.malmquist@gmail.com>
2019-10-28 13:25:32 +0000

    Fix error

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/statisticsIRUS/StatisticsIRUS.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/statisticsIRUS/StatisticsIRUS.java
@@ -55,7 +55,6 @@ public class StatisticsIRUS extends AbstractDSpaceTransformer
         pageMeta.addTrailLink(contextPath + "/", T_dspace_home);
         pageMeta.addTrail().addContent("IRUS Statistics");
 
-        authorizeService.isAdmin(context, dso)
         pageMeta.addMetadata("stylesheet", "screen", null, true).addContent("../../static/css/irus_stats.css");
         pageMeta.addMetadata("javascript", "static").addContent("static/js/irus_stats.js");
 
