e8d7802bee2c180011557279797a18c77dd65ea7
j4bbi <hrafn.malmquist@gmail.com>
2019-11-22 16:39:28 +0000

    Fix sidebar Hide empty divs and H2s

--- dspace-xmlui-mirage2/src/main/webapp/scripts/sidebar.js
+++ dspace-xmlui-mirage2/src/main/webapp/scripts/sidebar.js
@@ -19,7 +19,7 @@
 
         } else {
             $('#sidebar').show();
-            $('#sidebar').css('right', '-52%')
+            $('#sidebar').css('right', (document.body.clientWidth > 767) ? '-52%': '-100%')
             $('.row-offcanvas').addClass('active');
             $('.main-content').css('min-height', $('#sidebar').height());
             $('.main-content').on('click', toggle_sidebar);
