f2ffb3da87d8293ccfcd49cb324b5ef313b29927
j4bbi <hrafn.malmquist@gmail.com>
2020-02-10 12:27:31 +0000

    Submission form fixes continued

--- dspace-xmlui/src/main/webapp/static/js/hide_inputs.js
+++ dspace-xmlui/src/main/webapp/static/js/hide_inputs.js
@@ -1,6 +1,9 @@
 /*
 
-Hard-coded way to hide all the input elements on step 2 except the first two.
+Hard-coded way to
+ * hide citation on page 1
+ * hide all the input elements on page 2 except the first two.
+ * change header content on page 2 to "REF Information"
 
 */
 
@@ -11,9 +14,17 @@ var inputs_omitted = ["refterms_technicalException", "refterms_technicalExceptio
 "refterms_depositExceptionExplanation", "rioxxterms_publicationdate_year", "refterms_dateFCA_month",
 "refterms_dateEmbargoEnd_month", "refterms_dateFreeToRead_month", "refterms_dateFreeToDownload_month",
 "refterms_dateToSearch_month", "refterms_accessException", "refterms_accessExceptionExplanation",
-"refterms_exceptionFreeText"];
+"refterms_exceptionFreeText", "dc_identifier_citation"];
 
 
 for(var i = 0; i < inputs_omitted.length; i++)  {
-    $(input_append + inputs_omitted[i]).parents('div.ds-form-item.row').hide();
+    if($(input_append + inputs_omitted[i]).length) {
+        $(input_append + inputs_omitted[i]).parents('div.ds-form-item.row').hide();
+
+        if(inputs_omitted[i] != "dc_identifier_citation" && $(".ds-div-head.page-header.first-page-header").text().indexOf("Item submission") > -1)  {
+            $(".ds-div-head.page-header.first-page-header").text("REF Information");
+
+        }
+
+    }
 }
