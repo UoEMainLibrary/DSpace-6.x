418be6d3bbe851dc81b13b6018f8625b0ecb0f95
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2021-05-17 14:59:59 +0100

    Remove system out print from Atmire's IRUS patch

--- dspace/modules/atmire-statistics-exporter/atmire-statistics-exporter-api/src/main/java/com/atmire/statistics/export/ExportUsageEventListener.java
+++ dspace/modules/atmire-statistics-exporter/atmire-statistics-exporter-api/src/main/java/com/atmire/statistics/export/ExportUsageEventListener.java
@@ -317,7 +317,6 @@ public class ExportUsageEventListener extends AbstractUsageEventListener {
 
     private static void processUrl(Context c, String urlStr) throws IOException, SQLException {
         log.debug("Prepared to send url to tracker URL: " + urlStr);
-        System.out.println(urlStr);
         URLConnection conn;
 
         try {
